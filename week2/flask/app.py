035b3141c7a6815406aad712eb173d0830ff346c
mehul-anshumali <mehulanshumali.ma@gmail.com>
2021-03-15 12:13:56 +0530

    Modified main function

--- week2/flask/app.py
+++ week2/flask/app.py
@@ -175,5 +175,6 @@ def hostname_record():
     # print(host_record)
     return render_template('app_record.html', host_data=host_record)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
